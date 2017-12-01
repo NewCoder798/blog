a54bba8ae4fe209bf502d09c437575b8922f12a5
NewCoder798 <macuiling1995@163.com>
2017-12-01 16:46:37 +0800

    添加背景

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="layout">
     <header>
-        这是header
+      <canvas></canvas>
+      <div class="select">
+        <h1>Look For Less,Do More</h1>
+        <form action="">
+          <input type="text"/>
+          <input type="button"/>
+        </form>
+      </div>
     </header>
-    <nav>这是nav</nav>
+    <nav>
+      <div v-for="(item,index) in navList" :key="index">
+        {{ item.title }}
+      </div>
+    </nav>
     <section>这是内容</section>
   </div>
 </template>
@@ -13,13 +24,65 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      navList:[
+        {
+          title:'首页',
+          action:'/',
+        },
+        {
+          title:'文章',
+          action:'/',
+        },
+        {
+          title:'作品',
+          action:'/',
+        },
+        {
+          title:'关于',
+          action:'/',
+        }
+      ]
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="less">
+body{
+  overflow: hidden;
+  margin: 0;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-tap-highlight-color: transparent;
+}
+header{
+  position: relative;
+  padding-top:30%;
+  .select{
+    position: absolute;
+    left:50%;
+    top:50%;
+    width:500px;
+    margin-left:-250px;
+    color:#fff;
+    form{
+      input[type="text"]{
+        background: none;
+        border:2px solid #fff;
+        width:100%;
+        height: 45px;
+      }
+    }
+  }
+}
+canvas {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width:100% !important;
+  height:100% !important;
+  cursor: pointer;
+}
 </style>
